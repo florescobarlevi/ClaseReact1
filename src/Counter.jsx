d4861d39a8ba4d88f09d5306b91ff0553e342bb4
florescobarlevi <florenciaescobarlevi@gmail.com>
2022-09-14 22:06:12 -0300

    primer commit, en esta clase hciimos un contador que suma y resta, no llega a numeros negativos y tiene un reset que te lleva a cero

---
+++ src/Counter.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+
+import './counter.css';
+
+export const Counter = ( {title} ) =>{
+
+    const [counter, setCounter] = useState(0);
+
+    const incrementarContador = ( ) => {
+        setCounter(counter + 1)
+    }
+
+    const decrementoContador = ( ) => {
+        if ( counter <= 0){
+            counter(0)
+        }
+        setCounter(counter - 1)
+    }
+
+    const resetearContador = ( ) => {
+        setCounter(0)
+    }
+
+    return(
+        <div>
+            <h1>{ title }</h1>
+            <h2>{ counter }</h2>
+            <button onClick={incrementarContador}>+1</button>
+            <button onClick={decrementoContador}>-1</button>
+            <button onClick={resetearContador}>Reset</button>
+        </div>
+    )
+}
